4bd87f095909d2723ea97563ecd6d0a73ef5d90c
dennismimi <74968219+dennismimi@users.noreply.github.com>
2024-03-25 09:01:26 +0300

    Update Smote_Web.py

--- Smote_Web.py
+++ Smote_Web.py
@@ -26,35 +26,35 @@ st.write(""" ### Fill the Form Below for Prediction:money_with_wings:""")
 
 st.sidebar.selectbox('Explore or Predict',("Predict","Explore"))
 
-Education_list = ["Basic", "Primary", "Vocational", "Secondary", "Higher"]
-Marital_list = ["Married", "Cohabitant", "Single", "Divorced", "Widow"]
-EmploymentStatus = ["Unemployed", "Partially", "Fully", "Self", "Entrepreneur", "Retiree"]
-HomeTypeOwnership = ["Homeless", "Owner", "Living_With_Parents", "Tenant", "Prefurnished_Property",
+education_list = ["Basic", "Primary", "Vocational", "Secondary", "Higher"]
+marital_list = ["Married", "Cohabitant", "Single", "Divorced", "Widow"]
+employmentStatus = ["Unemployed", "Partially", "Fully", "Self", "Entrepreneur", "Retiree"]
+homeTypeOwnership = ["Homeless", "Owner", "Living_With_Parents", "Tenant", "Prefurnished_Property",
                      "Unfurnished_Property", "Joint_Tenant", "Joint_Ownership", "Mortgage", "Owner_with_Encumbrance",
                      "Other"]
-OccupationArea = ["Other", "Mining", "Processing", "Energies", "Utilities", "Construction",
+occupationArea = ["Other", "Mining", "Processing", "Energies", "Utilities", "Construction",
                   "Retail_and_Wholesale", "Transport_and_Warehousing", "Hospitality_and_Catering",
                   "Finance_and_Insurance", "Real_Estate", "Research", "Administrative", "Civil_Service_and_Military",
                   "Education", "Health_Care_and_Social_Help", "Arts_and_Entertainment",
                   "Agriculture_Forestry_and_Fishing"]
-UseOfLoan = ["Not_Set", "Loan_Consolidation", "Real_Estate", "Home_Improvement", "Business", "Education", "Travel",
+useOfLoan = ["Not_Set", "Loan_Consolidation", "Real_Estate", "Home_Improvement", "Business", "Education", "Travel",
              "Vehicle", "Other", "Health", "Finance_and_Insurance", "Research", "Administrative",
              "Civil_Service_and_Military", "Education_2", "Health_Care_and_Social_Help", "Arts_and_Entertainment",
              "Agriculture_Forestry_and_Fishing"]
-LoanDuration = ["less_than_a_month", "1 Month", "2_Months", "3_Months", "4_Months", "5_Months", "6_Months"]
+loanDuration = ["less_than_a_month", "1 Month", "2_Months", "3_Months", "4_Months", "5_Months", "6_Months"]
 
 col1, col2, = st.columns(2)
 
 Age = col1.slider('Enter Age',0,100)
 Gender = col2.radio("Select Gender",["Male", "Female", "Other"])
-MaritalStatus = col1.selectbox("Choose which best describes your Marital Status",Marital_list)
-Education = col1.selectbox("Education Level", Education_list)
+MaritalStatus = col1.selectbox("Choose which best describes your Marital Status",marital_list)
+Education = col1.selectbox("Education Level", education_list)
 NewCreditCustomer = col2.select_slider('Is This Your First Time Applying for Credit', ['Yes', 'No'])
-EmploymentStatus = col1.selectbox('Choose Which best describes your Employment Status', EmploymentStatus)
-HomeOwnershipType = col2.selectbox('Choose Which Best Describes Your Home',HomeTypeOwnership)
+EmploymentStatus = col1.selectbox('Choose Which best describes your Employment Status', employmentStatus)
+HomeOwnershipType = col2.selectbox('Choose Which Best Describes Your Home',homeTypeOwnership)
 EmploymentDurationCurrentEmployer = col1.slider('How Long have you been Employed',0,50)
-OccupationArea = col2.selectbox('Select Your Occupation Area', OccupationArea)
-LoanDuration = col1.select_slider("How long do you intend to take to pay off your credit", LoanDuration)
+OccupationArea = col2.selectbox('Select Your Occupation Area', occupationArea)
+LoanDuration = col1.select_slider("How long do you intend to take to pay off your credit", loanDuration)
 Interest = col2.number_input('The percentage of Interest')
 IncomeTotal = col1.number_input('Whats your Total Income?')
 NoOfPreviousLoansBeforeLoan = col2.slider('Number of Previous Loans',1,20)
@@ -84,24 +84,24 @@ df_pred = pd.DataFrame([[Age, LoanDuration, NewCreditCustomer, VerificationType,
                                 'Restructured', 'NoOfPreviousLoansBeforeLoan', 'CreditScoreEsMicroL', 'ModelVersion'])
 def transform(data):
     print(data)
-    if data in Marital_list:
-        return Marital_list.index(data)
-    elif data in Education_list:
-        return Education_list.index(data)
-    elif data in EmploymentStatus:
-        return EmploymentStatus.index(data)
-    elif data in OccupationArea:
-        return OccupationArea.index(data)
-    elif data in UseOfLoan:
-        return UseOfLoan.index(data)
-    elif data in LoanDuration:
-        return LoanDuration.index(data)
+    if data in marital_list:
+        return marital_list.index(data)
+    elif data in education_list:
+        return education_list.index(data)
+    elif data in employmentStatus:
+        return employmentStatus.index(data)
+    elif data in occupationArea:
+        return occupationArea.index(data)
+    elif data in useOfLoan:
+        return useOfLoan.index(data)
+    elif data in loanDuration:
+        return loanDuration.index(data)
     else:
         return 0
 df_pred['Gender'] = df_pred['Gender'].apply(lambda x: 1 if x == 'Male' else 0)
 df_pred['NewCreditCustomer'] = df_pred['NewCreditCustomer'].apply(lambda x: 1 if x == 'Yes' else 0)
 df_pred['Education'] = df_pred['Education'].apply(transform)
-df_pred['EmploymentStatus'] = df_pred['EmploymentStatus'].apply(lambda x: 0 if x == 'Unemployed' else EmploymentStatus.index(x))
+df_pred['EmploymentStatus'] = df_pred['EmploymentStatus'].apply(transform)
 df_pred['MaritalStatus'] = df_pred['MaritalStatus'].apply(transform)
 df_pred['HomeOwnershipType'] = df_pred['HomeOwnershipType'].apply(transform)
 df_pred['UseOfLoan'] = df_pred['UseOfLoan'].apply(transform)
